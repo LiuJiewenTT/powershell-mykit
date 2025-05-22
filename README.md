a5f743ea0631a3c50fa3bd7d71e9919d9dcc30b4
TTPeter <1635370493@qq.com>
2025-05-22 16:47:22 +0800

    重新组织

---
+++ README.md
@@ -0,0 +1,17 @@
+# PowerShell MyKit
+
+This is a collection of PowerShell scripts and modules that can be used to automate various tasks on Windows (basically for my own purposes).
+
+## Usages
+
+### Networking
+
+1. Dynamically set IP addresses of network interfaces.
+2. Show settings and status of **system proxy**.
+3. Show settings of **DNS servers**.
+4. Show **network connections** in a table format.
+
+## Setup
+
+Add `bin` directory to `%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1`. You may refer to this: [examples/setup/Microsoft.PowerShell_profile.ps1](examples/setup/Microsoft.PowerShell_profile.ps1).
+
---
+++ examples/setup/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,78 @@
+$mykit_dir = "G:\TT.OS.ToolKit\Windows\powershell-mykit\bin\"
+Write-Host "`$mykit_dir=${mykit_dir}"
+. (Join-Path -Path $mykit_dir -ChildPath "TT.LoadScript.utils.ps1")
+# . "${mykit_dir}load_powershell_mykit.bat"
+
+
+function profile_home {
+    ishome
+}
+
+function profile_normal {
+    isnormal
+}
+
+function profile_work {
+    iswork
+}
+
+
+function ishome {
+    $init_file = "TT.Home.init.ps1"
+
+    $filepath = Join-Path -Path $mykit_dir -ChildPath $init_file
+    echo "init_file path: $filepath"
+    # Load-ScriptFunctions $filepath
+    . $filepath
+
+    Home-NetInit
+}
+
+
+function isnormal {
+    $init_file = "TT.Normal.init.ps1"
+
+    $filepath = Join-Path -Path $mykit_dir -ChildPath $init_file
+    echo "init_file path: $filepath"
+    # Load-ScriptFunctions $filepath
+    . $filepath
+
+    Normal-NetInit
+}
+
+
+function iswork {
+    $init_file = "TT.Work1.init.ps1"
+
+    $filepath = Join-Path -Path $mykit_dir -ChildPath $init_file
+    echo "init_file path: $filepath"
+    Load-ScriptFunctions $filepath
+    # . $filepath
+
+    CTFF-NetInit
+}
+
+
+# @description 获取第一个匹配的网络适配器 Id
+# @param[input] 
+# @param[output] $global:adapterId 匹配到的网络适配器 Id
+function getIfAdapterIdExist {
+    # 获取所有网络适配器的 ID 列表
+    $adapterList = Get-NetAdapter | Select-Object -ExpandProperty ifIndex
+    $targetAdapterId_cnt = 2
+    $targetAdapterId_1 = 6   # 以太网
+    $targetAdapterId_2 = 18  # 以太网 2
+    # 使用 for 循环从 1 遍历到 cnt
+    for ($i = 1; $i -le $targetAdapterId_cnt; $i++) {
+        ${targetAdapterId_i} = (Get-Variable -Name "targetAdapterId_${i}").Value
+        Write-Host "尝试匹配第${i}个网络适配器, Id=${targetAdapterId_i}"
+        if ($adapterList -contains ${targetAdapterId_i}) {
+            Write-Host "找到的网络适配器Id是：${targetAdapterId_i}"
+            $global:adapterId = ${targetAdapterId_i}
+            break
+        }
+    }
+}
+
+
+getIfAdapterIdExist
